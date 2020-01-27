173a4249ca9800a82f12f6811490666ae3d169f6
gal <galchadad@gmail.com>
2020-01-27 14:52:35 +0200

    stage 1.2

---
+++ ex.py
@@ -0,0 +1,124 @@
+from pymongo import MongoClient
+
+from survey import insert_questions
+import logging
+import telegram
+from telegram import Update, ReplyKeyboardRemove
+from telegram.ext import CommandHandler, CallbackContext, MessageHandler, \
+    Filters, Updater, ConversationHandler
+import bot_settings
+
+logging.basicConfig(
+    format='[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+updater = Updater(token=bot_settings.BOT_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+
+def cancel(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Goodbye")
+
+
+def start(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    logger.info(f"> Start chat #{chat_id}")
+    custom_keyboard = ["כן", "לא"],
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    update.message.reply_text(""" 
+                             היי, האם תרצה להשתתף בסקר קצר לקראת הבחירות?
+                               ניתן לצאת באמצעות /cancel
+                           """,
+                              reply_markup=reply_markup)
+
+    return AGE
+
+
+AGE, GENDER, CITY, PARTY, FINISH = range(5)
+
+
+def age(update: Update, context: CallbackContext):
+    text = update.message.text
+    res["user_id"] = update.effective_chat.id
+    res["name"] = update.effective_chat.first_name
+    res["lname"] = update.effective_chat.last_name
+    res["want"] = text
+
+    custom_keyboard = ["18-30", "30-40", "40-50", "50-60"],
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    update.message.reply_text("בן כמה אתה?", reply_markup=reply_markup)
+    return GENDER
+
+
+def gender(update: Update, context: CallbackContext):
+    text = update.message.text
+    res["age"] = text
+
+    custom_keyboard = ["זכר", "נקבה", "אחר"],
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    update.message.reply_text("מה המין שלך?", reply_markup=reply_markup)
+    return CITY
+
+
+def city(update: Update, context: CallbackContext):
+    text = update.message.text
+    res["gender"] = text
+    update.message.reply_text("מהי עיר מגוריך?", reply_markup=ReplyKeyboardRemove())
+
+    return PARTY
+
+
+def party(update: Update, context: CallbackContext):
+    text = update.message.text
+    res["city"] = text
+    custom_keyboard = ["ליכוד", "כחול לבן", "שס", "ימינה"],
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    update.message.reply_text("באיזה מפלגה את\ה מתכוון לבחור?",
+                              reply_markup=reply_markup)
+
+    return FINISH
+
+
+def finish(update: Update, context: CallbackContext):
+    text = update.message.text
+    res["party"] = text
+    client = MongoClient()
+    db = client.get_database("survey")
+    results = db.get_collection("results")
+    results.insert_one(res)
+    print(res)
+    res.clear()
+    custom_keyboard = ["/start" , "/cancel"],
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    update.message.reply_text("תודה רבה שהשתתפת.",
+                              reply_markup=reply_markup)
+    return ConversationHandler.END
+
+
+conv_handler = ConversationHandler(
+
+    entry_points=[CommandHandler('start', start)],
+
+    states={
+        AGE: [MessageHandler(Filters.text, age)],
+
+        GENDER: [MessageHandler(Filters.text, gender)],
+
+        CITY: [MessageHandler(Filters.text, city)],
+
+        PARTY: [MessageHandler(Filters.text, party)],
+
+        FINISH: [MessageHandler(Filters.text, finish)],
+    },
+
+    fallbacks=[CommandHandler('cancel', cancel)]
+)
+dispatcher.add_handler(CommandHandler('cancel', cancel))
+dispatcher.add_handler(conv_handler)
+res = {}
+logger.info("* Start polling...")
+updater.start_polling()  # Starts polling in a background thread.
+updater.idle()  # Wait until Ctrl+C is pressed
+logger.info("* Bye!")
---
+++ rsvp.py
@@ -0,0 +1,102 @@
+import logging
+
+import telegram
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, CallbackContext, MessageHandler, \
+    Filters, Updater, CallbackQueryHandler, ConversationHandler
+
+import secret
+
+logging.basicConfig(
+    format='[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+updater = Updater(token=secret.BOT_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+info_about_event ="Joe's birthday picnic 2/2/2020, 17:00, Jabotinsky 25 Tel Aviv you can park at Arlozorov parking"
+
+
+def cancel(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Goodbye")
+
+
+def start(update: Update, context: CallbackContext):
+    keyboard = [[InlineKeyboardButton("not this time", callback_data='0')],
+                [InlineKeyboardButton("1", callback_data='1')],
+                [InlineKeyboardButton("2", callback_data='2')],
+                [InlineKeyboardButton("3", callback_data='3')],
+                [InlineKeyboardButton("4", callback_data='4')],
+                [InlineKeyboardButton("5", callback_data='5')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    user_name = update.message.from_user.first_name
+    chat_id = update.effective_chat.id
+    logger.info(f"> Start chat #{chat_id}")
+    welcome_message = 'Hi, {} Joe wants to know if you attend:{}'.format(user_name, info_about_event)
+    update.message.reply_text(welcome_message, reply_markup=reply_markup)
+    return COMING_OR_NOT
+
+
+COMING_OR_NOT, WHAT_TO_BRING, FINISH = range(3)
+list_of_stuff = ['bisly', 'cake', 'beer']
+
+
+def coming_or_not(update: Update, context: CallbackContext):
+    print("Hello")
+    query = update.callback_query
+    chat_id = update.effective_chat.id
+    name = update.effective_user.first_name
+    goodbye_massege = 'sorry to here your not coming {}, hope i see you soon'.format(name)
+    coming_massege = "see you soon {}, you will arrive as:{} pepole\n what would like to bring?".format(name, query.data)
+    if query.data == '0':
+        context.bot.send_message(chat_id=chat_id, text=goodbye_massege)
+    else:
+        keyboard = []
+        for item in list_of_stuff:
+            print('hi')
+            keyboard.append([InlineKeyboardButton(item, callback_data=item)])
+        context.bot.send_message(chat_id=chat_id, reply_markup=InlineKeyboardMarkup(keyboard), text=coming_massege)
+        return WHAT_TO_BRING
+
+
+def what_to_bring(update, context):
+    user_id = update.effective_chat.id
+    what_user_bring = update.callback_query
+    print(what_user_bring.data)
+    print(user_id)
+
+
+def finish():
+    pass
+
+
+def respond(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    text = update.message.text
+    logger.info(f"= Got on chat #{chat_id}: {text!r}")
+    response = update.message.from_user.first_name
+    context.bot.send_message(chat_id=update.message.chat_id, text=response)
+
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)],
+
+    states={
+        COMING_OR_NOT: [CallbackQueryHandler(coming_or_not)],
+        WHAT_TO_BRING: [CallbackQueryHandler(what_to_bring)],
+        FINISH: [CallbackQueryHandler(finish)],
+
+            },
+    fallbacks=[CommandHandler('cancel', cancel)]
+)
+dispatcher.add_handler(CommandHandler('cancel', cancel))
+
+dispatcher.add_handler(conv_handler)
+
+echo_handler = MessageHandler(Filters.text, respond)
+dispatcher.add_handler(echo_handler)
+logger.info("* Start polling...")
+updater.start_polling()  # Starts polling in a background thread.
+updater.idle()  # Wait until Ctrl+C is pressed
+logger.info("* Bye!")
