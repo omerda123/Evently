c6a74a3979d9053bbdd99997061ff30857172e80
Omer Daniel <omerda@gmail.com>
2020-01-27 09:37:55 +0200

    add args to /start

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import logging
 
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, \
-    Filters, Updater
+    Filters, Updater, ConversationHandler
 
 import secrets
 
@@ -16,25 +16,107 @@ updater = Updater(token=secrets.BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 
+def extract_unique_code(text):
+    # Extracts the unique_code from the sent /start command.
+    return text.split()[1] if len(text.split()) > 1 else None
+
+
 def start(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    logger.info(f"> Start chat #{chat_id}")
-    context.bot.send_message(chat_id=chat_id, text="💣 Welcome! 💣")
+    if context.args:
+        print(context.args)
+    else:
+        chat_id = update.effective_chat.id
+        logger.info(f"> Start chat #{chat_id}")
+        context.bot.send_message(chat_id=chat_id,
+                                 text=""" Welcome! 💣
+            Hi, to create a event please type /create_event
+                                                       """)
 
 
-def respond(update: Update, context: CallbackContext):
+def create_event(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
+    logger.info(f"> create event #{chat_id}")
+    context.bot.send_message(chat_id=chat_id,
+                             text="""Please enter all info about the event below in one line:
+            example: Joe's birthday picnic 2/2/2020, 17:00, Jabotinsky 25 Tel Aviv you can park at Arlozorov parking""")
+    return EVENT_CREATED
+
+
+def event_created(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    logger.info(f"> create event #{chat_id}")
     text = update.message.text
-    logger.info(f"= Got on chat #{chat_id}: {text!r}")
-    response = text.replace("7", "💣")
-    context.bot.send_message(chat_id=update.message.chat_id, text=response)
+    context.bot.send_message(chat_id=chat_id,
+                             text=""" Event created ! share the following message with your friends to RSVP You are invited to:
+Joe's birthday picnic 2/2/2020, 17:00, Jabotinsky 25 Tel Aviv you can park at Arlozorov parking
+click here to RSVP:  t.me/event_handler_bot?event=123
+if you want to tell your friend items to bring please write /add_list
+""")
 
+    return ConversationHandler.END
+
+
+def get_items(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    logger.info(f"> create event #{chat_id}")
+    context.bot.send_message(chat_id=chat_id,
+                             text="""please write the stuff you need for the event followed by #end""")
+
+
+def insert_items(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    logger.info(f"> create event #{chat_id}")
+    context.bot.send_message(chat_id=chat_id,
+                             text="""item inserted""")
+    return GET_ITEMS
+
+
+def cancel(args):
+    pass
+
+
+def event(update: Update, context: CallbackContext):
+    print(context.args)
+    chat_id = update.effective_chat.id
+    context.bot.send_message(chat_id=chat_id, text=context.args[0])
+
+    # for arg in args:
+
+
+CREATION, EVENT_CREATED, = range(2)
+GET_ITEMS, INSERT_ITEMS = range(2)
+
+create_event_handler = ConversationHandler(
+
+    entry_points=[CommandHandler('create_event', create_event)],
+
+    states={
+        CREATION: [MessageHandler(Filters.text, create_event)],
+
+        EVENT_CREATED: [MessageHandler(Filters.text, event_created)],
+    },
+
+    fallbacks=[CommandHandler('cancel', cancel)]
+)
+
+add_item_handler = ConversationHandler(
+
+    entry_points=[CommandHandler('add_list', get_items)],
+
+    states={
+        GET_ITEMS: [MessageHandler(Filters.text, get_items)],
+
+        INSERT_ITEMS: [MessageHandler(Filters.text, insert_items)],
+    },
+
+    fallbacks=[CommandHandler('cancel', cancel)]
+)
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-
-echo_handler = MessageHandler(Filters.text, respond)
-dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(create_event_handler)
+dispatcher.add_handler(add_item_handler)
+dispatcher.add_handler(CommandHandler('event', event, pass_args=True))
 
 logger.info("* Start polling...")
 updater.start_polling()  # Starts polling in a background thread.
